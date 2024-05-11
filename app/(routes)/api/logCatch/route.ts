77bf988041b10e4e12d8087e53c202219c141681
loo-y <loo-y@outlook.com>
2024-05-11 16:59:31 +0800

    feat: add edge runtime for logCatch api

--- app/(routes)/api/logCatch/route.ts
+++ app/(routes)/api/logCatch/route.ts
@@ -1,6 +1,8 @@
 import _ from 'lodash'
 import { NextRequest, NextResponse } from 'next/server'
 
+export const runtime = 'edge'
+
 export async function POST(request: NextRequest) {
     const body = await request.json()
     const { info, type } = body || {}
